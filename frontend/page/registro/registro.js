bf888782dceea5df8ce095c3957dee040b9b4e44
BenjaminGonzalez98 <benjamincho2004@gmail.com>
2024-08-23 21:39:04 -0300

    aaaa

---
+++ frontend/page/registro/registro.js
@@ -0,0 +1,21 @@
+
+
+
+// EN EL REGISTRO PARA QUE EL INPUT SE PONGA VERDE CUANDO LA CONTRASEÑA ES VALIDA
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('contraseña');
+
+    passwordInput.addEventListener('input', function() {
+        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        
+        if (pattern.test(passwordInput.value)) {
+            passwordInput.classList.add('valid');
+            passwordInput.classList.remove('invalid');
+        } else {
+            passwordInput.classList.add('invalid');
+            passwordInput.classList.remove('valid');
+        }
+    });
+});
