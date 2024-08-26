f0b22765ae5887b4584cca3f3804a7893caafe77
emiigonzalezzz <168480956+emiigonzalezzz@users.noreply.github.com>
2024-08-26 17:24:09 -0300

    ....

--- backend/Model/UsuarioDAO.php
+++ backend/Model/UsuarioDAO.php
@@ -3,21 +3,21 @@ require_once "../Connection/Connection.php";
 
 class Usuario{ 
 
-    function GuardarUsuarioModel() 
+    function GuardarUsuarioModel($id_usuario, $nombrecompleto, $email, $contraseña) 
     {
-         $sql = "INSERT INTO ebanking() VALUES();";
+         $sql = "INSERT INTO usuario(id_usuario, nombrecompleto, email, contraseña) VALUES($id_usuario, $nombrecompleto, $email, $contraseña);";
          $connection = connection();
          $respuesta = $connection->query($sql);
          return $respuesta;
          if ($respuesta == false){
             if ($connection->errno == 1060){
-                $respuesta=$this->ActualizarUsuarioModel();
+                $respuesta=$this->ActualizarContraseñaModel($contraseña, $email);
             }
         }
     }
 
-    function ActualizarUsuarioModel(){
-        $sql = "UPDATE  SET  = '$',  = '$', = '$',  = $ WHERE  = $;"; 
+    function ActualizarContraseñaModel($contraseña, $email){
+        $sql = "UPDATE usuario SET contraseña = '$contraseña' WHERE email = '$email';"; 
         $connection = connection();
         $respuesta = $connection->query($sql);
         return $respuesta;
